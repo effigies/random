d0601a7247aae2b6b0ebe5b49fb4a73a36f2208e
Christopher J. Markiewicz <effigies@gmail.com>
2015-04-13 10:10:21 -0400

    Add documentation, a demonstrative object

--- python/ObjectPool.py
+++ python/ObjectPool.py
@@ -1,12 +1,30 @@
 import threading
 import types
 
+
 class Pool(list):
     """Thread-safe stack of objects not currently in use, generates new object
-    when empty.
+    when empty. Objects have a release() method that reinserts them into the
+    pool.
 
     Use as a decorator. Decorated classes must have init() method to
-    prepare them for reuse."""
+    prepare them for reuse.
+
+    Currently renders pydoc useless.
+
+    Example:
+
+    @Pool
+    class myClass(object):
+        def __init__(self, ...):
+            ...
+
+        def init(self):
+            ...
+
+    obj = myClass()
+    obj.release()
+    """
     def __init__(self, klass):
         super(Pool, self).__init__()
 
@@ -20,3 +38,10 @@ class Pool(list):
             obj = self.pop() if self else self.klass()
         obj.init()
         return obj
+
+
+@Pool
+class TestPooling(object):
+    """Basic object that contains x attribute, default value 1"""
+    def init(self):
+        self.x = 1

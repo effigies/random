ed9d94eeeac6572e929e55f455647567ebe79c1d
Christopher J. Markiewicz <effigies@gmail.com>
2015-07-08 22:00:00 -0400

    Decorator function pool as alternative to class Pool

--- python/ObjectPool.py
+++ python/ObjectPool.py
@@ -1,5 +1,6 @@
 import threading
 import types
+import warnings
 
 
 class Pool(list):
@@ -38,6 +39,49 @@ class Pool(list):
             obj = self.pop() if self else self.klass()
         obj.init()
         return obj
+
+
+def pool(klass):
+    """Thread-safe pool of objects not currently in use, generates new object
+    when empty.
+
+    Use as a decorator. Decorated classes must have init() method to
+    prepare them for reuse."""
+    lock = threading.Lock()
+    pool = set()
+
+    orig_new = klass.__new__
+    orig_init = klass.__init__
+
+    def __new__(cls, *args, **kwargs):
+        "Get object from pool, generating a new one if empty"
+        with lock:
+            if pool:
+                obj = pool.pop()
+                obj._used = True
+                return obj
+        return orig_new(cls, *args, **kwargs)
+    klass.__new__ = classmethod(__new__)
+
+    def __init__(self, *args, **kwargs):
+        if hasattr(self, '_used'):
+            self.init()
+            del self._used
+            return
+
+        orig_init(self, *args, **kwargs)
+    klass.__init__ = __init__
+
+    def release(self):
+        """Release for reuse"""
+        if self in pool:
+            warnings.warn(RuntimeWarning('Attempting double-release of ' +
+                                         klass.__name__))
+        else:
+            pool.add(self)
+    klass.release = release
+
+    return klass
 
 
 @Pool

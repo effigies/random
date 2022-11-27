2a9dc037b5c181c6d0d03e955000ceebf0037b0a
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-11-27 10:12:42 -0500

    FIX: Use single session to avoid rate limiting, add debug logging

--- python/tickers.py
+++ python/tickers.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python
-# 
+#
 # Chris Markiewicz 2020
-# 
+#
 # Inspired by http://finance.jasonstrimpel.com/bulk-stock-download/
-# 
+#
 # Stripped down to bare-bones, but with additional features:
-# 
+#
 # 1) Dates are parsed and saved as dates, not strings
 # 2) Values rounded to nearest cent
 # 3) May output to ODS, XLSX or print to screen
 import argparse
+import logging
 import io
+from functools import lru_cache, partial
 from pathlib import Path
 from datetime import datetime, timedelta
 from dateutil.parser import parse as parsedate
 import requests
 import pandas as pd
-from functools import lru_cache, partial
 
 
 YAHOO_API = "https://query1.finance.yahoo.com/v7/finance/download/{symbol}"
+SESSION = requests.Session()
+SESSION.headers.update(
+    {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/91.0.4472.124 Safari/537.36"
+    }
+)
 
 
 def get_historical_data(symbol, start_date, end_date):
@@ -30,15 +39,13 @@ def get_historical_data(symbol, start_date, end_date):
         "events": "history",
         "includeAdjustedClose": "true",
     }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
-                      "AppleWebKit/537.36 (KHTML, like Gecko) "
-                      "Chrome/91.0.4472.124 Safari/537.36"
-    }
-    res = requests.get(YAHOO_API.format(symbol=symbol),
-                       params=params, headers=headers)
+    logging.getLogger("tickers").info(f"Retrieving {symbol}")
+    res = SESSION.get(YAHOO_API.format(symbol=symbol), params=params)
     if res.ok:
         return pd.read_csv(io.BytesIO(res.content), index_col="Date", parse_dates=True)
+    else:
+        logging.getLogger("tickers").debug(f"Request: {res.request}")
+        logging.getLogger("tickers").debug(f"Response: {res}")
     raise ValueError(f"Could not retrieve symbol {symbol}")
 
 
@@ -71,12 +78,18 @@ def main():
     )
     parser.add_argument("-o", "--output", metavar="PATH")
     parser.add_argument("-n", "--fillna", choices=("forward", "none"))
+    parser.add_argument("-v", "--verbose", action="count", default=0)
     parser.add_argument("start_date", nargs="?", help="First date (default 1 week ago)")
     parser.add_argument("end_date", nargs="?", help="Final date (default today)")
     opts = parser.parse_args()
 
+    logging.basicConfig(level=("WARNING", "INFO", "DEBUG")[opts.verbose])
+
     end = parsedate(opts.end_date) if opts.end_date else datetime.today()
     start = parsedate(opts.start_date) if opts.start_date else end - timedelta(weeks=1)
+
+    if end < start:
+        raise ValueError(f"Start date ({start}) cannot come after end date ({end})")
 
     sym_path = Path(opts.symbols)
     if sym_path.exists():
